24ecaf4abab36e564b05405c9450d571d3d8f924
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-11 16:12:31 +0000

    header section done

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './components/header/header.component';
+import {RouterModule} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+  ],
+  exports: [
+    HeaderComponent,
+    MatIconModule,
+  ]
+})
+export class CoreModule {
+}
