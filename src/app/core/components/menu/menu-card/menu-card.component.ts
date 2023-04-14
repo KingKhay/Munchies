70d576f04655650cac4785b5dc74995ff5c0c2bb
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-14 08:57:03 +0000

    fixed mat icon bug with the menu card component

--- src/app/core/components/menu/menu-card/menu-card.component.ts
+++ src/app/core/components/menu/menu-card/menu-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Food} from "../../../../shared/models/Food";
 
 @Component({
   selector: 'app-menu-card',
   templateUrl: './menu-card.component.html',
   styleUrls: ['./menu-card.component.css']
 })
-export class MenuCardComponent {
+export class MenuCardComponent implements OnInit {
+
+  @Input()
+  food: Food = {id: '1', name: '', price: 2,stars: 5, imageUrl:'assets/images/menu1.jpg',tags:['FastFood'],cookTime: '',favorite: true};
+
+  ngOnInit(): void {
+  }
+
 
 }
