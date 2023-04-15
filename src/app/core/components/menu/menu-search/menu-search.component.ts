020a6d5389aa1aa97f7e570041781e17480b3054
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-15 07:16:15 +0000

    styled search bar component

--- src/app/core/components/menu/menu-search/menu-search.component.ts
+++ src/app/core/components/menu/menu-search/menu-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu-search',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class MenuSearchComponent {
 
+  constructor(private router: Router) {
+  }
+
+  searchTerm = '';
+
+
+  search() {
+    if (this.searchTerm.trim() === ''){
+      this.router.navigateByUrl('/menu')
+    }
+    this.router.navigateByUrl(`/search/${this.searchTerm}`);
+  }
 }
