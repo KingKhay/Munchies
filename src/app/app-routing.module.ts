d4e796bca159e76e78b659a6243ccf96e10a981a
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-17 10:02:28 +0000

    added the login route to the app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {HeroComponent} from "./core/components/hero/hero.component";
 import {ErrorComponent} from "./core/components/error/error.component";
 import {MenuComponent} from "./core/components/menu/menu.component";
+import {LoginComponent} from "./core/components/login/login.component";
 
 const routes: Routes = [
   {path: '', component: HeroComponent},
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'search/:searchTerm', component: MenuComponent},
   {path: 'tag', component: MenuComponent},
   {path: 'tag/:tagName', component: MenuComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', component: ErrorComponent}
 ];
 
